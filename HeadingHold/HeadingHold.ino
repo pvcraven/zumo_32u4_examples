f332fb16151d232f96187b633b3d047cbf045ddb
Paul Vincent Craven <paul@cravenfamily.com>
2016-08-12 16:03:52 -0500

    Updates for improved accuracy.

--- HeadingHold/HeadingHold.ino
+++ HeadingHold/HeadingHold.ino
@@ -11,10 +11,10 @@ Zumo32U4ButtonB buttonB;
 Zumo32U4ButtonC buttonC;
 
 // Motor speeds while turning
-int turnSpeed = 160;
+int turnSpeed = 140;
 
 // Max motor speed
-int motorSpeed = 450;
+int motorSpeed = 400;
 
 // Min motor speed as we get close
 int minSpeed = 140;
@@ -59,32 +59,34 @@ void forward(long count) {
     int angle = (((int32_t)turnAngle >> 16) * 360) >> 16;
     int distanceLeft = count - countsLeft;
 
+    
     // --- Calculate our speed
     // Not up to full speed, but a long way to go
     // Accelerate!
     if(curSpeed < motorSpeed && distanceLeft >= slowDownDistance) {
-        curSpeed += acceleration;
-        if(curSpeed > motorSpeed)
-          curSpeed = motorSpeed;        
+      curSpeed += acceleration;
+      if(curSpeed > motorSpeed)
+        curSpeed = motorSpeed;        
     }
 
     // Not up to min speed
     // Accelerate!
     else if(curSpeed < minSpeed) {
-        curSpeed += acceleration;
-        if(curSpeed > minSpeed)
-          curSpeed = minSpeed;        
+      curSpeed += acceleration;
+      if(curSpeed > minSpeed)
+        curSpeed = minSpeed;        
     } 
 
     // Getting close, and going fast
     // Slow down!
-    else if(curSpeed > minSpeed) {
-        curSpeed -= acceleration;
-        if(curSpeed < minSpeed)
-          curSpeed = minSpeed;        
-      
+    else if(distanceLeft < slowDownDistance) {
+      lcd.gotoXY(0, 0);
+      int newSpeed = motorSpeed - (slowDownDistance - distanceLeft) / (float)slowDownDistance * (motorSpeed - minSpeed);
+      if(newSpeed < curSpeed) {
+        curSpeed = newSpeed;
+      }
     }
-
+    
     // Set the motor speed.
     // We adjust the speed according to our angle so we keep the same heading.
     motors.setSpeeds(curSpeed + (angle * 5), curSpeed - (angle * 5));
@@ -93,8 +95,10 @@ void forward(long count) {
     lcd.gotoXY(0, 0);
     lcd.print(angle);
     lcd.print(" ");
+    lcd.print(curSpeed);
+    lcd.print(" ");
     lcd.gotoXY(0, 1);
-    lcd.print(countsLeft);
+    lcd.print(distanceLeft);
     lcd.print(" ");
     delay(1);
   };
@@ -121,7 +125,7 @@ void turnLeft(int degrees) {
     lcd.print(" ");
 
     // If we get close, slow down our turn rate
-    if ( abs(degrees - cur_angle) < 25) {
+    if ( abs(degrees - cur_angle) < 30) {
       motors.setSpeeds(-turnSpeed / 2, turnSpeed / 2);
     }    
     delay(1);
@@ -168,15 +172,15 @@ void loop() {
   if(buttonPress) {
     delay(500);
     turnLeft(90);
-    lcd.clear();
+    //lcd.clear();
   }
 
   // If we press A, turn right.
   buttonPress = buttonB.getSingleDebouncedPress();
   if(buttonPress) {
     delay(500);
-    turnRight(90);
-    lcd.clear();
+    forward(3000);
+    //lcd.clear();
   }
 
 
